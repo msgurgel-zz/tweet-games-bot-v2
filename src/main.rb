b378ca0d3407fe2e540faab5ff2a8ad184379f20
Mateus Gurgel <mgurgel5855@conestogac.on.ca>
2020-02-27 12:50:22 -0500

    :sparkles: Add interrupt handling

--- src/main.rb
+++ src/main.rb
@@ -24,7 +24,15 @@ end
 
 # Stream tweets?
 topics = ["@TweetGamesBot"]
-stream.filter( track: topics.join(",") ) do |object|
-    log.info(object.text) if object.is_a? Twitter::Tweet
-    twitter.update("@#{object.user.screen_name} Thanks for mentioning me ♥️🤖", in_reply_to_status: object)
+
+
+log.info("start listening for mentions...")
+begin
+    stream.filter( track: topics.join(",") ) do |object|
+        log.info(object.text) if object.is_a? Twitter::Tweet
+        twitter.update("@#{object.user.screen_name} Thanks for mentioning me ♥️🤖", in_reply_to_status: object)
+    end
+rescue Interrupt
+    stream.close
+    log.info("shutting down")
 end
