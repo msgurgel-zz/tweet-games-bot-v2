8312708c2f92ea22c177740eaea7c6aecadb73f5
Mateus Gurgel <mgurgel5855@conestogac.on.ca>
2020-02-29 13:33:14 -0500

    :sparkles: Handle SIGTERM

--- app/main.rb
+++ app/main.rb
@@ -3,10 +3,16 @@ require 'dotenv/load'
 require 'logger'
 require 'twitter'
 
+# Turn off log buffering (for Heroku)
 $stdout.sync = true
 log = Logger.new(STDOUT)
 log.level = Logger::DEBUG
 
+def shut_down
+
+end
+
+# Connect to Twitter
 stream = Twitter::Streaming::Client.new do |config|
     config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
     config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
@@ -21,6 +27,20 @@ twitter = Twitter::REST::Client.new do |config|
     config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
 
+# Handle SIGTERM
+Signal.trap("TERM") {
+    stream.close
+    puts "received SIGTERM. shutting down"
+    exit
+}
+
+# Handle SIGINT
+Signal.trap("INT") {
+    stream.close
+    puts "received SIGINT. shutting down"
+    exit
+}
+
 log.info("start listening for mentions...")
 
 topics = ["@TweetGamesBot"]
@@ -30,9 +50,6 @@ begin
         timeNow = Time.now.strftime("%H:%M:%S")
         twitter.update("@#{object.user.screen_name} Thanks for mentioning me ♥️🤖 Time: #{timeNow}", in_reply_to_status: object)
     end
-rescue Interrupt
-    stream.close
-    log.info("shutting down")
 rescue JSON::ParserError => e
     if e.message == "767: unexpected token at 'Exceeded connection limit for user'"
         log.fatal("you just got rate limited! err = #{e}")
